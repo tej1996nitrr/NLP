a85249cda39dab099e320825dbacb60292f5e713
tejas1996nitrr <tejaskanikdaley1996@gmail.com>
2020-05-14 22:51:51 +0530

    Reading Tagged Corpora

--- 4.Tagging.py
+++ 4.Tagging.py
@@ -6,6 +6,25 @@ IN-preposition
 NN-Noun
 JJ-adjective
 VBP-present tense verb
+ADJ     adjective              new, good, high, special, big, local
+ADV     adverb              really, already, still, early, now
+CNJ     conjunction              and, or, but, if, while, although
+DET     determiner              the, a, some, most, every, no
+EX     existential              there, there’s
+FW     foreign word              dolce, ersatz, esprit, quo, maitre
+MOD     modal verb              will, can, would, may, must, should
+N     noun              year, home, costs, time, education
+NP     proper noun              Alison, Africa, April, Washington
+NUM     number              twenty-four, fourth, 1991, 14:24
+PRO     pronoun              he, their, her, its, my, I, us
+P     preposition              on, of, at, with, by, into, under
+TO     the              word to to
+UH     interjection              ah, bang, ha, whee, hmpf, oops
+V     verb              is, has, get, do, make, see, run
+VD     past tense              said, took, told, made, asked
+VG     present participle              making, going, playing, working
+VN     past participle              given, taken, begun, sung
+WH     wh determiner              who, which, when, what, where, how
 
 """
 import nltk
@@ -44,5 +63,20 @@ said/VBD ``/`` ARE/BER well/QL operated/VBN and/CC follow/VB generally/RB
 accepted/VBN practices/NNS which/WDT inure/VB to/IN the/AT best/JJT
 interest/NN of/IN both/ABX governments/NNS ''/'' ./"""
 [nltk.tag.str2tuple(t) for t in sent.split()]
+
+# %%
+nltk.corpus.brown.tagged_words()
+nltk.corpus.nps_chat.tagged_words()
+
+# %%
+nltk.corpus.indian.tagged_words()
+
+# %%
+# most common tags in the news category of the Brown Corpus
+from nltk.corpus import brown
+brown_news_tagged = brown.tagged_words(categories='news', simplify_tags=True)
+tag_fd = nltk.FreqDist(tag for (word, tag) in brown_news_tagged)
+tag_fd.keys()
+
 
 # %%
