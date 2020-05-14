45f9e959fe545677d73793a97dd2280cf997e06a
tejas1996nitrr <tejaskanikdaley1996@gmail.com>
2020-05-14 22:46:09 +0530

    representing tagged corpora

--- 4.Tagging.py
+++ 4.Tagging.py
@@ -33,3 +33,16 @@ text.similar('over')
 text.similar('the')
 
 # %%
+tagged_token = nltk.tag.str2tuple('fly/NN')
+tagged_token
+# %%
+sent = """
+The/AT grand/JJ jury/NN commented/VBD on/IN a/AT number/NN of/IN
+other/AP topics/NNS ,/, AMONG/IN them/PPO the/AT Atlanta/NP and/CC
+Fulton/NP-tl County/NN-tl purchasing/VBG departments/NNS which/WDT it/PPS
+said/VBD ``/`` ARE/BER well/QL operated/VBN and/CC follow/VB generally/RB
+accepted/VBN practices/NNS which/WDT inure/VB to/IN the/AT best/JJT
+interest/NN of/IN both/ABX governments/NNS ''/'' ./"""
+[nltk.tag.str2tuple(t) for t in sent.split()]
+
+# %%
