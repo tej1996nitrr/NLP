4a6cd58e3fdc7b3287f0414ca8ed9cc462634583
tejas1996nitrr <tejaskanikdaley1996@gmail.com>
2020-05-19 23:27:50 +0530

    dev set

--- 5.Text classification.py
+++ 5.Text classification.py
@@ -27,3 +27,36 @@ test_set = apply_features(gender_features, names[:500])
 nltk.classify.accuracy(classifier, test_set)
 
 # %%
+#developement set
+train_names = names[1500:]
+devtest_names = names[500:1500]
+test_names = names[:500]
+train_set = [(gender_features(n), g) for (n,g) in train_names]
+devtest_set = [(gender_features(n), g) for (n,g) in devtest_names]
+test_set = [(gender_features(n), g) for (n,g) in test_names]
+classifier = nltk.NaiveBayesClassifier.train(train_set)
+print (nltk.classify.accuracy(classifier, devtest_set))
+
+# %%
+errors = []
+for (name, tag) in devtest_names:
+    guess = classifier.classify(gender_features(name))
+    if guess != tag:
+        errors.append( (tag, guess, name) )
+
+# %%
+for (tag, guess, name) in sorted(errors): # doctest: +ELLIPSIS +NORMALIZE_WHITESPACE
+    print ('correct=%-8s guess=%-8s name=%-30s' %(tag, guess, name))
+
+# %%
+def gender_features(word):
+    return {'suffix1': word[-1:],
+            'suffix2': word[-2:]}
+
+# %%
+train_set = [(gender_features(n), g) for (n,g) in train_names]
+devtest_set = [(gender_features(n), g) for (n,g) in devtest_names]
+classifier = nltk.NaiveBayesClassifier.train(train_set)
+print (nltk.classify.accuracy(classifier, devtest_set))
+
+# %%
